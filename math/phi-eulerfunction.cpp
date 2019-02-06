90f8fe2e6194d83134d991c874a8cf1827abe9d4
Mariano Piatti <30485597+mariano-piatti@users.noreply.github.com>
2019-02-05 21:34:13 -0300

    phi added

---
+++ math/phi-eulerfunction.cpp
@@ -0,0 +1,14 @@
+ll phi(ll n) {
+	if(n==0)return 0;
+    ll result = n;
+    for (ll i = 2; i * i <= n; i++) {
+        if(n % i == 0) {
+            while(n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if(n > 1)
+        result -= result / n;
+    return result;
+}
