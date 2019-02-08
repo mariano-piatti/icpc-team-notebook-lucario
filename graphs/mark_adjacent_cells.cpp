be2362ae85d3d5dd6083276e1ae4293cd38dcbf6
Mariano Piatti <30485597+mariano-piatti@users.noreply.github.com>
2019-02-08 13:55:26 -0300

    added mark_adjacent_cells

---
+++ graphs/mark_adjacent_cells.cpp
@@ -0,0 +1,32 @@
+void mark(int i,int j){
+	if(i==0&&j==0){
+		g[i][j].pb(mp(i,j+1));
+		g[i][j].pb(mp(i+1,j));
+	}else if(j==0&&i<n-1){
+		g[i][j].pb(mp(i+1,j));
+		g[i][j].pb(mp(i-1,j));
+		g[i][j].pb(mp(i,j+1));
+	}else if(j==0&&i==n-1){
+		g[i][j].pb(mp(i-1,j));
+		g[i][j].pb(mp(i,j+1));
+	}else if(i==0&&j<m-1){
+		g[i][j].pb(mp(i+1,j));
+		g[i][j].pb(mp(i,j+1));
+		g[i][j].pb(mp(i,j-1));
+	}else if(i==0&&j==m-1){
+		g[i][j].pb(mp(i,j-1));
+		g[i][j].pb(mp(i+1,j));
+	}else if(j==m-1&&i<n-1){
+		g[i][j].pb(mp(i+1,j));
+		g[i][j].pb(mp(i,j-1));
+		g[i][j].pb(mp(i-1,j));
+	}else if(j==m-1&&i==n-1){
+		g[i][j].pb(mp(i,j-1));
+		g[i][j].pb(mp(i-1,j));
+	}else{
+		g[i][j].pb(mp(i+1,j));
+		g[i][j].pb(mp(i,j+1));
+		g[i][j].pb(mp(i,j-1));
+		g[i][j].pb(mp(i-1,j));
+	}
+}
